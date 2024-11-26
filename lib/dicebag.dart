4f284a943eb1695474a23baa6475c25f74752aa1
Blade Lucas <elffe2@hotmail.ca>
2024-11-26 18:49:10 -0500

    Widgets added to dicebag.dart (Not styled)

--- lib/dicebag.dart
+++ lib/dicebag.dart
@@ -9,16 +9,54 @@ class DiceBag extends StatefulWidget {
 }
 
 class _DiceBagState extends State<DiceBag> {
+  String dropdownvalue = "D6";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Menu()));
-            },
-            child: const Text("Home")),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Dice Bag"),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              DropdownButton(
+                  items: const [
+                    DropdownMenuItem(child: Text("D4"), value: "D4"),
+                    DropdownMenuItem(child: Text("D6"), value: "D6"),
+                    DropdownMenuItem(child: Text("D8"), value: "D8"),
+                    DropdownMenuItem(child: Text("D10"), value: "D10"),
+                    DropdownMenuItem(child: Text("D12"), value: "D12"),
+                    DropdownMenuItem(child: Text("D20"), value: "D20"),
+                  ],
+                  value: dropdownvalue,
+                  onChanged: (String? value) {
+                    setState(() {
+                      dropdownvalue = value!;
+                    });
+                  }),
+              const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Number of Dice"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Roll"),
+              ),
+              const Text(
+                "You Rolled a [NUMBER]",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Menu()));
+                },
+                child: const Text("Home"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -33,9 +33,6 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
