7281bbc0a7fb077f87f66bd0af1de721000b773d
Blade Lucas <elffe2@hotmail.ca>
2024-11-27 01:26:49 -0500

    Implemented dice rolling logic for multiple dice and different kinds of dice

--- lib/dicebag.dart
+++ lib/dicebag.dart
@@ -1,5 +1,12 @@
 import 'package:dndassistantapp/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:math';
+
+String dropdownvalue = "6";
+int numberOfDice = 1;
+bool diceRolled = false;
+List<int> rolls = <int>[];
 
 class DiceBag extends StatefulWidget {
   const DiceBag({super.key});
@@ -8,8 +15,36 @@ class DiceBag extends StatefulWidget {
   State<DiceBag> createState() => _DiceBagState();
 }
 
+List<int> roll() {
+  int diceSelected = int.parse(dropdownvalue);
+  var rng = Random();
+  List<int> diceRolls = <int>[];
+  for (int i = 0; i < numberOfDice; i++) {
+    diceRolls.add(rng.nextInt(diceSelected) + 1);
+  }
+  diceRolled = true;
+  return diceRolls;
+}
+
+String stringBuilder() {
+  String message = "You Rolled a ";
+  for (int i = 0; i < rolls.length; i++) {
+    if (rolls.length == 1) {
+      message += rolls[i].toString();
+    } else {
+      if (i != rolls.length - 1) {
+        message += "${rolls[i]}, ";
+      } else {
+        message += rolls[i].toString();
+      }
+    }
+  }
+  return message;
+}
+
 class _DiceBagState extends State<DiceBag> {
-  String dropdownvalue = "D6";
+  TextEditingController numberOfDiceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,12 +57,12 @@ class _DiceBagState extends State<DiceBag> {
             children: <Widget>[
               DropdownButton(
                   items: const [
-                    DropdownMenuItem(child: Text("D4"), value: "D4"),
-                    DropdownMenuItem(child: Text("D6"), value: "D6"),
-                    DropdownMenuItem(child: Text("D8"), value: "D8"),
-                    DropdownMenuItem(child: Text("D10"), value: "D10"),
-                    DropdownMenuItem(child: Text("D12"), value: "D12"),
-                    DropdownMenuItem(child: Text("D20"), value: "D20"),
+                    DropdownMenuItem(value: "4", child: Text("D4")),
+                    DropdownMenuItem(value: "6", child: Text("D6")),
+                    DropdownMenuItem(value: "8", child: Text("D8")),
+                    DropdownMenuItem(value: "10", child: Text("D10")),
+                    DropdownMenuItem(value: "12", child: Text("D12")),
+                    DropdownMenuItem(value: "20", child: Text("D20")),
                   ],
                   value: dropdownvalue,
                   onChanged: (String? value) {
@@ -35,18 +70,33 @@ class _DiceBagState extends State<DiceBag> {
                       dropdownvalue = value!;
                     });
                   }),
-              const TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: "Number of Dice"),
-              ),
+              TextFormField(
+                  controller: numberOfDiceController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Number of Dice"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  onChanged: (value) {
+                    numberOfDice = int.tryParse(value) ?? 1;
+                  }),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    rolls.clear();
+                    rolls = roll();
+                  });
+                },
                 child: const Text("Roll"),
               ),
-              const Text(
-                "You Rolled a [NUMBER]",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
+              Visibility(
+                  visible: diceRolled,
+                  child: Text(
+                    stringBuilder(),
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  )),
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(context,
